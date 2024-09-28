71b82622653cde81b9979685f242339b5aef4ba3
manojthamilarasu <manojthamilarasu@gmail.com>
2024-09-28 13:55:04 +0530

    add testcase to check comma and newline delimiter

--- models/calculator.test.js
+++ models/calculator.test.js
@@ -11,3 +11,11 @@ test('Return 0 for undefined input', () => {
 test('Return 0 for number input', () => {
     expect(add(12)).toBe(0);
 });
+
+test('Add up comma seperated numbers', () => {
+    expect(add("1,2,3,4")).toBe(10);
+});
+
+test('Support New line as delimiter', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
